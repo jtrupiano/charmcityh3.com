eb8620cb4a9aa7dc6b2dda7dc8c502462934746f
John Trupiano <jtrupiano@gmail.com>
2011-05-26 08:41:23 -0400

    Make navbar actually work, add link to mailing list

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -3,5 +3,12 @@ function showSong(id) {
 }
 
 $(function() {
+$('#nav_body').click(function() { scrollTo('body'); });
+$('#nav_next_trail').click(function() { scrollTo('next_trail'); });
+$('#nav_hymnal').click(function() { scrollTo('hymnal'); });
   showSong(1);
 });
+
+function scrollTo(id) {
+  $('html,body').animate({scrollTop: $("#"+id).offset().top-45},'slow');
+}
